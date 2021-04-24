7da044933631108dc68acca0313b696871a15ef7
Hamza Eraoui <hamzaprofessionnel1234@gmail.com>
2021-04-24 23:50:39 +0000

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'vendor/phpmvc-framework',
+  ),
+  'versions' => 
+  array (
+    'vendor/phpmvc-framework' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
