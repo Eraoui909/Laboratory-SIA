1333e5a092938c19d713cee8241a6249ae626a72
ZaimAchraf <achraf.zaime@gmail.com>
2021-04-28 00:08:50 +0200

    headerSlider

---
+++ Views/home.php
@@ -0,0 +1,16 @@
+<div class="header">
+
+    <div class="slider1 slider active"></div>
+    <div class="slider2 slider"></div>
+    <div class="slider3 slider"></div>
+
+    <div class="pagination">
+        <ul>
+            <li class="active"></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</div>
+
+<script src="/js/HomeScript.js"></script>
