a75620662aef564d2a675b55febd1237a4b75a34
ZaimAchraf <achraf.zaime@gmail.com>
2021-05-13 04:32:52 +0200

    navbar responsive

--- Views/masterLayout/navBar.php
+++ Views/masterLayout/navBar.php
@@ -5,7 +5,7 @@
         <ul>
             <li class="keep icone" id="menu-btn-icon"><i class="fas fa-bars"></i></li>
             <li><a href="/"><?= $lang['home'] ?></a></li>
-            <li class="account-link keep">
+            <li>
                 <a href="#"><?= $lang['presentation'] ?></a>
                 <div class="drop-down">
                     <a href="/motDePresident"><?= $lang['moDePresident'] ?></a>
@@ -25,11 +25,11 @@
         <ul>
             <li><a href=""><?= $lang['aboutUs']?></a></li>
             <li><a href="/contact"><?= $lang['contact'] ?></a></li>
-            <li class="account-link keep">
+            <li class="account-link keep lang">
                 <?php
                     if (isset($_SESSION['token']))
                         echo '
-                            <a href="">' . $lang['account'] . '</a>
+                            <a href="#"><span class="hide">' . $lang['account'] . '</span> <span class="show"><i class="fas fa-user-alt"></i></span></a>
                             <div class="drop-down">
                                 <a href="/logout">' . $lang['logout'] . '</a>
                                 <a href="/teacher/profile">' . $lang['settings'] . ' </a>
@@ -40,8 +40,8 @@
                 ?>
 
             </li>
-            <li class="account-link keep">
-                <a href=""><i class="fas fa-globe"></i> lang <i class="fas fa-caret-down"></i></a>
+            <li class="account-link keep lang">
+                <a href="#"><i class="fas fa-globe"></i> <span class="hide">lang</span> <i class="fas fa-caret-down"></i></a>
                 <div class="drop-down">
                     <a href="/lang/fr"><?= $lang['fr']?></a>
                     <a href="/lang/en"><?= $lang['en']?></a>
@@ -53,10 +53,17 @@
     <div class="menu" id="menuRespBar">
         <ul>
             <li><a href="/"><?= $lang['home'] ?></a></li>
-            <li><a href="/contact"><?= $lang['contact'] ?></a></li>
+
+            <li class="presentation"><a href="#"><?= $lang['presentation'] ?> <i style="padding: 8px 0 0 4px; font-size: 25px" class="fas fa-angle-down"></i></a></li>
+            <li class="presentation-drop-down hidden drop"><a href="/motDePresident"><?= $lang['moDePresident'] ?> </a></li>
+            <li class="presentation-drop-down hidden drop"><a href="/presentation"> presentation de labo </a></li>
+            <li class="presentation-drop-down hidden drop"><a href="/conditionSoutnance"><?= $lang['conditionDeSoutnance'] ?></a></li>
+
             <li><a href=""><?= $lang['enseignant'] ?></a></li>
             <li><a href=""><?= $lang['doctorant'] ?></a></li>
             <li><a href=""><?= $lang['aboutUs']?></a></li>
+            <li><a href="/contact"><?= $lang['contact'] ?></a></li>
+
         </ul>
     </div>
 </nav>
