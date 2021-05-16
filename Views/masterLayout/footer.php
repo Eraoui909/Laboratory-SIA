0522b23e7eef94eadcb5120869ceb6fefa982fdf
Hamza Eraoui <hamzaprofessionnel1234@gmail.com>
2021-05-16 15:23:35 +0100

    footer

--- Views/masterLayout/footer.php
+++ Views/masterLayout/footer.php
@@ -1,19 +1,27 @@
 <div class="ha-footer">
     <div class="ha-footer-head">
 
-        <div class="ha-footer-head-sec ha-footer-head-sec1">
+        <div class="ha-footer-head-sec ha-footer-head-sec1" style="width: 150%">
             <h3>SIA LAB</h3>
-            <img src="<?php dirname(__DIR__) ?>/Storage/Statics/images/logoLaboFst.png" width="150px" height="100px">
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam aliquid aperiam architecto </h4>
+            <img src="<?php dirname(__DIR__) ?>/Storage/Statics/images/logoLaboFst.png" width="200px" height="100px">
+            <h4>Le laboratoire LSIA (Laboratoire des Systèmes Intelligents & Applications) a été accrédité la première fois en 2011 au sein de la Faculté des Sciences et Techniques de Fès, CED Sciences et Techniques de l’Ingénieur,
+                Université Sidi Mohammed Ben Abdellah. Il est composé de 18 enseignants chercheurs permanents et 40 doctorants.</h4>
 
         </div>
         <div class="ha-footer-head-sec ha-footer-head-sec2">
-            <h3>Site Map</h3>
+            <h3><?= $lang['presentation'] ?></h3>
             <ul>
-                <li>Link 1</li>
-                <li>Link 1</li>
-                <li>Link 1</li>
-                <li>Link 1</li>
+                <li>
+                    <a href="/motDePresident"><?= $lang['moDePresident'] ?></a>
+                </li>
+                <li>
+                    <a href="/presentation"><?= $lang['desc&historique'] ?></a>
+                </li>
+                <li>
+                    <a href="/conditionSoutnance"><?= $lang['conditionDeSoutnance'] ?></a>
+                </li>
+
+
             </ul>
         </div>
         <div class="ha-footer-head-sec ha-footer-head-sec3">
@@ -28,12 +36,47 @@
             </ul>
         </div>
         <div class="ha-footer-head-sec ha-footer-head-sec4">
-            <h3>Site Map</h3>
+            <h3>Collaboration</h3>
             <ul>
-                <li>Link 1</li>
-                <li>Link 1</li>
-                <li>Link 1</li>
-                <li>Link 1</li>
+                <li>
+                    <img src="/Storage/Statics/images/u-picardie.png"  width="30px" height="30px" style="border-radius: 50%">
+                    <a href="https://www.u-picardie.fr/"  target="_blank">  UPJV, Amiens France</a>
+                </li>
+                <li>
+                    <img src="/Storage/Statics/images/umonpellier.png"  width="30px" height="30px" style="border-radius: 50%">
+                    <a href="https://www.umontpellier.fr/en/"  target="_blank">  Université de Montpelier</a>
+                </li>
+                <li>
+                    <img src="/Storage/Statics/images/CNR-ILC_Logo.png"  width="30px" height="30px" style="border-radius: 50%">
+                    <a href="http://www.ilc.cnr.it/"  target="_blank">  ILC-CNR de Pise en Italie</a>
+                </li>
+
+                <li>
+                    <img src="/Storage/Statics/images/Logo_CHU-final.png"  width="30px" height="30px" style="border-radius: 50%">
+                    <a href="http://www.chu-fes.ma/en/home-en/"  target="_blank">  CHU de FES</a>
+                </li>
+
+                <li>
+                    <img src="/Storage/Statics/images/ul-logo.jpg"  width="30px" height="30px" style="border-radius: 50%">
+                    <a href="https://www.ul.ie/"  target="_blank">  Université de Limerick Irlande</a>
+                </li>
+
+                <li>
+                    <img src="/Storage/Statics/images/fsdm-logo.png"  width="30px" height="30px" style="border-radius: 50%">
+                    <a href="http://www.fsdmfes.ac.ma/"  target="_blank"> Laboratoire IMS de FSDM de Fes</a>
+                </li>
+
+                <li>
+                    <img src="/Storage/Statics/images/BL_logo.png"  width="30px" height="30px" style="border-radius: 50%">
+                    <a href="https://www.berger-levrault.com/ma/"  target="_blank"> Entreprise Berger-Levrault Montpelier</a>
+                </li>
+
+                <li>
+                    <img src="/Storage/Statics/images/amis-logo.png"  width="30px" height="30px" style="border-radius: 50%">
+                    <a href="http://ww2.fmp-usmba.ac.ma/blog/amiis/"  target="_blank"> Association Marocaine d’Ingénierie et d’Innovation en Santé (AMIIS).</a>
+                </li>
+
+
             </ul>
         </div>
 
@@ -43,7 +86,7 @@
 
     <div class="ha-footer-foot">
         <div class="ha-footer-foot-left">
-            Copyright 2021 | LabSIA
+            Copyright <?php echo date("Y")?> | LabSIA
         </div>
 
     </div>
